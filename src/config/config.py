fb6d8f14af7429c159ff495b23980e25af85c30c
VictorPedroza <victor.pedroza@protonmail.com>
2026-03-12 16:30:24 -0300

    :zap: Update Config with button params

--- src/config/config.py
+++ src/config/config.py
@@ -15,4 +15,25 @@ class Config:
     ctk.set_appearance_mode(theme)
     ctk.set_default_color_theme("dark-blue")
     
+    # Fontes
+    font_title = ("Roboto", 40, "bold")
+    font_display = ("Roboto", 32)
+    font_button = ("Roboto", 18, "bold")
     
+    # Botões da calculadora
+    buttons = [
+        ['C', '⌫', '%', '÷'],
+        ['7', '8', '9', '×'],
+        ['4', '5', '6', '−'],
+        ['1', '2', '3', '+'],
+        ['±', '0', '.', '=']
+    ]
+    
+    # Mapeamento de operadores
+    operations = {
+        '÷': '/',
+        '×': '*',
+        '−': '-',
+        '+': '+',
+        '%': '%'
+    }
